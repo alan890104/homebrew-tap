eb989cd90119d096aa52f979ae1ff34eebb216b1
Yu Lun Hsu <alan890104@gmail.com>
2026-03-14 23:07:14 +0800

    Fix SHA256 checksums to match actual release downloads

--- Formula/unsee.rb
+++ Formula/unsee.rb
@@ -7,20 +7,20 @@ class Unsee < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/alan890104/unsee/releases/download/v#{version}/unsee-aarch64-apple-darwin.tar.gz"
-      sha256 "a8b0c08cb81baa1a97a4f5ac8887e1005731d59abc81fabf94fed59a044570f7"
+      sha256 "e8e3094fd5f3ae98fad56e8cb099ac9ac45a2396d151ba6f6a2506f7cbb49fc9"
     else
       url "https://github.com/alan890104/unsee/releases/download/v#{version}/unsee-x86_64-apple-darwin.tar.gz"
-      sha256 "5442b1aad4e7fcc0d5ca7f6e4ce17f96b52d0779358cce98be9e92c69cb76dba"
+      sha256 "cf5b39abe8320d672a587a9c4c21a7ae6a0e6a929ca00269691d3416630e94c6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/alan890104/unsee/releases/download/v#{version}/unsee-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "889708ac769b2020018912675d4a5885ff26354e94a3a8ed6c4ba315fe8d64ff"
+      sha256 "24e0cbbbab14ecb0c5a5c1b69d6d81548e95f77f6a774c7685175e22ffabefab"
     else
       url "https://github.com/alan890104/unsee/releases/download/v#{version}/unsee-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0776a9bc24dd8dfcf0f3282c52e7c1ed1b4f4717bec1c3a76ebe87021de21a71"
+      sha256 "6064c7fe0344389e9af2dd0a76d3b3c9d9ddb4a598571a404d8bb23e26998429"
     end
   end
 
