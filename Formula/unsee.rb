4753c2627f1c89d8bf4014a135c39be200c3336b
Yu Lun Hsu <alan890104@gmail.com>
2026-03-14 22:50:26 +0800

    Add unsee formula v0.1.0

---
+++ Formula/unsee.rb
@@ -0,0 +1,29 @@
+class Unsee < Formula
+  desc "Credential protection for LLM coding agents"
+  homepage "https://github.com/alan890104/unsee"
+  url "https://github.com/alan890104/unsee/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "a1742c09c8c1245e8cde708507db1a2722e5fb9795ffa69b75060d1aac736f87"
+  license "MIT"
+
+  head "https://github.com/alan890104/unsee.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "crates/unsee-cli")
+  end
+
+  def caveats
+    <<~EOS
+      To activate protection, run:
+        unsee install
+
+      Before uninstalling unsee, remove shell wrappers first:
+        unsee uninstall
+    EOS
+  end
+
+  test do
+    assert_match "Credential protection", shell_output("#{bin}/unsee --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# Homebrew Tap
+
+Homebrew formulae for [unsee](https://github.com/alan890104/unsee) and other tools.
+
+## Install
+
+```bash
+brew tap alan890104/tap
+brew install unsee
+```
